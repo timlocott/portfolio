aa3a71779842cf60f5d6ea8fae375468cfde3456
timlocott <tsam.cottrell@gmail.com>
2024-10-09 16:58:31 -0600

    Built out structure for Skill tags on exp

--- frontend/src/components/Exp.jsx
+++ frontend/src/components/Exp.jsx
@@ -4,7 +4,7 @@ function WorkExp(){
     return(
         <div className="flex flex-col items-center justify-center h-screen m-4">
             <div className="flex flex-row justify-around w-full flex-wrap">
-                <ExpCard company="University of Utah NAL Lab" dateRange="Aug 2023 - May 2024" title="Mobile Application Developer"/>
+                <ExpCard company="University of Utah NAL Lab" dateRange="Aug 2023 - May 2024" title="Mobile Application Developer" skills={["Flutter","Python","AWS"]}/>
                 <ExpCard company="Tempus Ex Machina" dateRange="Aug 2023 - Dec 2023" title="Analyst"/>
                 <ExpCard company="L3 Harris Technologies" dateRange="Jun 2022 - Dec 2022" title="Software Engineering Intern"/>
                 <ExpCard company="University of Utah" dateRange="Feb 2021 - Jun 2022" title="Salesforce Intern"/>
--- frontend/src/components/ExpCard.jsx
+++ frontend/src/components/ExpCard.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-function ExpCard({company, dateRange, title}){
+function ExpCard({company, dateRange, title, skills = []}){
     return (
         <div className="flip-card m-4">
-            <div className="flip-card-inner rounded-xl hover:cursor-pointer text-ebony flex flex-col">
+            <div className="flip-card-inner rounded-xl hover:cursor-pointe flex flex-col">
                 <div className="flip-card-front rounded-xl p-2">
                     <div className="mt-2">
                         { company }
@@ -15,7 +15,13 @@ function ExpCard({company, dateRange, title}){
                     </div>
                 </div>
                 <div className="flip-card-back rounded-xl p-2">
-                    <h1>John Doe</h1>
+                    <div className="flex flex-row">
+                        {skills.map((item, index) => (
+                            <div className="bg-white rounded-xl text-ebony w-fit px-2 mr-2" key={index}>
+                                {item}
+                            </div>
+                        ))}
+                    </div>
                     <p>Architect & Engineer</p>
                     <p>We love that guy</p>
                 </div>
