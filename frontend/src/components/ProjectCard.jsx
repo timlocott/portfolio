71b07b9b3175abcb4a2354b7342c314c5d4a774f
timlocott <tsam.cottrell@gmail.com>
2024-10-19 08:44:37 -0600

    Hide images till loaded

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import OrbitProgress from 'react-loading-indicators/OrbitProgress';
+
 
 function ProjectCard({imgSrc, projTitle, textColor}){
+    const [imageLoaded, setImageLoaded] = useState(false);
+    {!imageLoaded && (
+                <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
+                    <OrbitProgress variant="disc" color="#575C55" size="medium" text="" textColor=""/>
+                </div>
+            )}
     return (
-        <div className="relative inline-block text-center m-4 hover:text-white max-w-48 max-h-64 h-64" role="listitem" onClick={() => {
-                // setProject(projTitle)
-                console.log("CLICKED!!!!!!!")
-            }}>
-            <img className={`block h-full rounded-xl border border-ebony hover:cursor-pointer shadow hover:shadow-2xl hover:brightness-50`} src={imgSrc}/>
+        <div className={`relative inline-block text-center m-4 hover:text-white max-w-48 max-h-64 h-64" role="listitem ${imageLoaded ? '' : 'invisible'}`}>
+            <img className={`block h-full rounded-xl border border-ebony hover:cursor-pointer shadow hover:shadow-2xl hover:brightness-50`} src={imgSrc} onLoad={() => setImageLoaded(true)}/>
             <div className={`absolute transform right-6 top-56 ${textColor} text-right text-shadow hover:cursor-pointer w-36 inline-block align-top`}>
                 {projTitle}
             </div>
--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable no-unused-vars */
 
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { useState } from 'react';
-import OrbitProgress from 'react-loading-indicators/OrbitProgress';
 
 import uReact from '../assets/UReactFull.png'
 import LMSWebsite from '../assets/LMSWebsite.jpg'
@@ -13,12 +12,9 @@ import fieldHistoryTracking from '../assets/fieldHistoryTracking.jpg'
 import chatClient from '../assets/chatClient.jpg'
 import photoGallery from '../assets/photoGallery.jpg'
 import portLogo from '../assets/portLogo.png'
-
-const ProjectCard = lazy(() => import('./ProjectCard'));
-import ProjectInfo from './ProjectInfo';
+import ProjectCard from './ProjectCard';
 
 function Projects(){
-    const [project, setProject] = useState("")
     return(
         <div className="px-6 flex flex-col h-screen text-black overflow-visib;e">
             <div className="flex flex-row w-full justify-center text-white my-6 bg-transparent sticky">
@@ -28,22 +24,17 @@ function Projects(){
             </div>
             <div className="flex flex-col mx-10">
                 <div className="flex flex-row flex-wrap justify-center h-full" role="list">
-                    <Suspense fallback={<OrbitProgress variant="disc" color="#575C55" size="medium" text="" textColor=""/>}>
-                        <ProjectCard imgSrc={portLogo} textColor="text-white" projTitle="Personal Website"/>
-                        <ProjectCard imgSrc={photoGallery} textColor="text-white" projTitle="Photo Gallery"/>
-                        <ProjectCard imgSrc={uReact} textColor="text-white" projTitle="UReact"/>
-                        <ProjectCard imgSrc={chatClient} textColor="text-white" projTitle="Chat Client"/>
-                        <ProjectCard imgSrc={LMSWebsite} textColor="text-white" projTitle="LMS Website"/>
-                        <ProjectCard imgSrc={databaseImg} textColor="text-white" projTitle="LMS Database"/>
-                        <ProjectCard imgSrc={lifestyleApp} textColor="text-white" projTitle="LifeStyle App"/>
-                        <ProjectCard imgSrc={custContactSearch} textColor="text-white" projTitle="LWC Search"/>
-                        <ProjectCard imgSrc={fieldHistoryTracking} textColor="text-white" projTitle="Field History"/>
-                    </Suspense>
+                    <ProjectCard imgSrc={portLogo} textColor="text-white" projTitle="Personal Website"/>
+                    <ProjectCard imgSrc={photoGallery} textColor="text-white" projTitle="Photo Gallery"/>
+                    <ProjectCard imgSrc={uReact} textColor="text-white" projTitle="UReact"/>
+                    <ProjectCard imgSrc={chatClient} textColor="text-white" projTitle="Chat Client"/>
+                    <ProjectCard imgSrc={LMSWebsite} textColor="text-white" projTitle="LMS Website"/>
+                    <ProjectCard imgSrc={databaseImg} textColor="text-white" projTitle="LMS Database"/>
+                    <ProjectCard imgSrc={lifestyleApp} textColor="text-white" projTitle="LifeStyle App"/>
+                    <ProjectCard imgSrc={custContactSearch} textColor="text-white" projTitle="LWC Search"/>
+                    <ProjectCard imgSrc={fieldHistoryTracking} textColor="text-white" projTitle="Field History"/>
                 </div>
             </div>
-            {project != "" &&
-                <ProjectInfo project={project}/>
-            }
         </div>
     );
 }
